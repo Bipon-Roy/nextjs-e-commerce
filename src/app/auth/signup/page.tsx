63e7a595bf5b7fa476641d7e99b57e5878882682
Bipon Roy <biponroy400@gmail.com>
2024-03-18 21:27:17 +0600

    Added bcrypt to store password securly

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -29,7 +29,7 @@ const SignUp = () => {
                         },
                         body: JSON.stringify(values),
                     });
-                    console.log(res);
+
                     if (res.ok) {
                         alert("User Created Successfully");
                     }
--- src/app/models/userModel.ts
+++ src/app/models/userModel.ts
@@ -1,5 +1,5 @@
 import { Document, Model, Schema, model, models } from "mongoose";
-
+import { genSalt, hash } from "bcrypt";
 interface UserDocument extends Document {
     email: string;
     name: string;
@@ -20,6 +20,19 @@ const userSchema = new Schema<UserDocument>(
     },
     { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) {
+            return next();
+        }
+        const salt = await genSalt(10);
+        this.password = await hash(this.password, salt);
+        next();
+    } catch (error) {
+        throw error;
+    }
+});
 
 const userModel = models.User || model("User", userSchema);
 
