1ba6f874b0a99d101e67554c4065360ab39c0b8e
Bipon Roy <biponroy400@gmail.com>
2024-03-19 13:03:59 +0600

    Added email verification logic to signup route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,12 +3,21 @@ import { NewUserRequest } from "@/types";
 import { NextResponse } from "next/server";
 import userModel from "@models/userModel";
 import nodemailer from "nodemailer";
+import EmailVerificationToken from "@models/emailVerification";
+import crypto from "crypto";
 export const POST = async (req: Request) => {
     const body = (await req.json()) as NewUserRequest;
     await startDb();
     const newUser = await userModel.create({ ...body });
 
     // console.log(await newUser.comparePassword("12345678"));
+
+    const token = crypto.randomBytes(36).toString("hex");
+
+    await EmailVerificationToken.create({
+        user: newUser._id,
+        token,
+    });
 
     var transport = nodemailer.createTransport({
         host: "sandbox.smtp.mailtrap.io",
@@ -19,10 +28,12 @@ export const POST = async (req: Request) => {
         },
     });
 
+    const verificationUrl = `http://localhost:3000/verify?token=${token}&userId=${newUser._id}`;
+
     await transport.sendMail({
         from: "verification@nextjsecom.com",
         to: newUser.email,
-        html: `<h1>Please verify your email by clicking on <a href='http://localhost:3000'>this link</a></h1>`,
+        html: `<h1>Please verify your email by clicking on <a href="${verificationUrl}">this link</a></h1>`,
     });
 
     return NextResponse.json(newUser);
