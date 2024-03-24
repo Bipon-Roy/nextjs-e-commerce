1176b6233e49540a771d387b92d1e7bcc4acc54e
Bipon Roy <biponroy400@gmail.com>
2024-03-24 17:25:58 +0600

    Implement group routing

---
+++ src/app/(guest_routes)/layout.tsx
@@ -0,0 +1,17 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { ReactNode } from "react";
+
+interface Props {
+    children: ReactNode;
+}
+const GuestLayout = async ({ children }: Props) => {
+    const session = await auth();
+    if (session) {
+        return redirect("/");
+    }
+
+    return <div>{children}</div>;
+};
+
+export default GuestLayout;
