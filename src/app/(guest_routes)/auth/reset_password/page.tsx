4a6e1b062f6b429008ad1d07bbad508b3fdcc1e0
Bipon Roy <biponroy400@gmail.com>
2024-03-30 00:01:28 +0600

    Updated functionality on reset password route

--- src/app/(guest_routes)/auth/reset_password/page.tsx
+++ src/app/(guest_routes)/auth/reset_password/page.tsx
@@ -1,70 +1,38 @@
-"use client";
 import React from "react";
-import FormContainer from "@components/AuthFormContainer";
-import { Button, Input } from "@material-tailwind/react";
-import { useFormik } from "formik";
-import * as yup from "yup";
-
-const validationSchema = yup.object().shape({
-    password1: yup
-        .string()
-        .required("Password is required")
-        .min(8, "Password must be at least 8 characters long"),
-    password2: yup
-        .string()
-        .oneOf([yup.ref("password1")], "Passwords must match")
-        .required("Confirm password is required"),
-});
+import PasswordResetTokenModel from "@/app/models/passwordReset";
+import startDb from "@/app/lib/db";
+import { notFound } from "next/navigation";
+import UpdatePassword from "@/components/UpdatePassword";
 
 interface Props {
-    userId: string;
-    token: string;
-}
-const ResetPassword = () => {
-    const { values, isSubmitting, touched, errors, handleSubmit, handleBlur, handleChange } =
-        useFormik({
-            initialValues: { password1: "", password2: "" },
-            validationSchema,
-            onSubmit: async (values, actions) => {},
-        });
-
-    const { password1, password2 } = values;
-    type valueKeys = keyof typeof values;
-    const error = (name: valueKeys) => {
-        return errors[name] && touched[name] ? true : false;
+    searchParams: {
+        token: string;
+        userId: string;
     };
-    return (
-        <FormContainer title="Reset password" onSubmit={handleSubmit}>
-            <Input
-                crossOrigin={undefined}
-                name="password1"
-                label="Password"
-                value={password1}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={error("password1")}
-                type="password"
-            />
-            <Input
-                crossOrigin={undefined}
-                name="password2"
-                label="Confirm Password"
-                value={password2}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={error("password2")}
-                type="password"
-            />
-            <Button
-                placeholder={undefined}
-                type="submit"
-                className="w-full"
-                disabled={isSubmitting}
-            >
-                Reset Password
-            </Button>
-        </FormContainer>
-    );
+}
+const fetchTokenValidation = async (token: string, userId: string) => {
+    await startDb();
+    const resetToken = await PasswordResetTokenModel.findOne({ user: userId });
+    if (!resetToken) return null;
+
+    const matched = await resetToken.compareToken(token);
+    if (!matched) return null;
+
+    return true;
+};
+
+const ResetPassword = async ({ searchParams }: Props) => {
+    const { token, userId } = searchParams;
+    if (!token && !userId) {
+        return notFound();
+    }
+    const isValid = await fetchTokenValidation(token, userId);
+
+    if (isValid) {
+        return notFound();
+    }
+
+    return <UpdatePassword token={token} userId={userId} />;
 };
 
 export default ResetPassword;
---
+++ src/components/UpdatePassword.tsx
@@ -0,0 +1,77 @@
+"use client";
+import React from "react";
+import FormContainer from "@components/AuthFormContainer";
+import { Button, Input } from "@material-tailwind/react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+    password1: yup
+        .string()
+        .required("Password is required")
+        .min(8, "Password must be at least 8 characters long"),
+    password2: yup
+        .string()
+        .oneOf([yup.ref("password1")], "Passwords must match")
+        .required("Confirm password is required"),
+});
+
+interface Props {
+    userId: string;
+    token: string;
+}
+
+const UpdatePassword = ({ token, userId }: Props) => {
+    const { values, isSubmitting, touched, errors, handleSubmit, handleBlur, handleChange } =
+        useFormik({
+            initialValues: { password1: "", password2: "" },
+            validationSchema,
+            onSubmit: async (values, actions) => {},
+        });
+
+    const { password1, password2 } = values;
+    type valueKeys = keyof typeof values;
+    const error = (name: valueKeys) => {
+        return errors[name] && touched[name] ? true : false;
+    };
+    return (
+        <FormContainer title="Reset password" onSubmit={handleSubmit}>
+            <Input
+                crossOrigin={undefined}
+                name="password1"
+                label="Password"
+                value={password1}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={error("password1")}
+                type="password"
+            />
+            <span className=" text-red-500">
+                {errors.password1 && touched.password1 && errors.password1}
+            </span>
+            <Input
+                crossOrigin={undefined}
+                name="password2"
+                label="Confirm Password"
+                value={password2}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={error("password2")}
+                type="password"
+            />
+            <span className=" text-red-500">
+                {errors.password2 && touched.password2 && errors.password2}
+            </span>
+            <Button
+                placeholder={undefined}
+                type="submit"
+                className="w-full"
+                disabled={isSubmitting}
+            >
+                Reset Password
+            </Button>
+        </FormContainer>
+    );
+};
+
+export default UpdatePassword;
