6afeb2453cf2ed769f5d98a177605a1e3ebd3c88
Bipon Roy <biponroy400@gmail.com>
2024-04-08 22:32:48 +0600

    added functionality to show validation error msg

--- src/app/(admin)/products/create/page.tsx
+++ src/app/(admin)/products/create/page.tsx
@@ -2,9 +2,23 @@
 
 import AddProductForm from "@/components/AddProductForm";
 import { NewProductInfo } from "@/types";
+import { newProductInfoSchema } from "@/utils/validationSchema";
+import { toast } from "react-toastify";
+import { ValidationError } from "yup";
 
 const AddProduct = () => {
-    const handleAddProduct = (values: NewProductInfo) => {};
+    const handleAddProduct = async (values: NewProductInfo) => {
+        try {
+            await newProductInfoSchema.validate(values, { abortEarly: false });
+        } catch (error) {
+            if (error instanceof ValidationError) {
+                // console.log(error.inner);
+                error.inner.map((err) => {
+                    toast.error(err.message);
+                });
+            }
+        }
+    };
     return (
         <>
             <AddProductForm onSubmit={handleAddProduct} />
