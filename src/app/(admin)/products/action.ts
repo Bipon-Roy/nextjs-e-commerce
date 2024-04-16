c038c21bd9ec08926bca2625d96e106a9d313a60
Bipon Roy <biponroy400@gmail.com>
2024-04-16 21:08:51 +0600

    Added removeImage from cloudinary server functionality

--- src/app/(admin)/products/action.ts
+++ src/app/(admin)/products/action.ts
@@ -51,3 +51,17 @@ export const createProduct = async (info: NewProduct) => {
 export const removeImageFromCloud = async (publicId: string) => {
     await cloudinary.uploader.destroy(publicId);
 };
+
+export const removeAndUpdateProductImage = async (id: string, publicId: string) => {
+    try {
+        const { result } = await cloudinary.uploader.destroy(publicId);
+        if (result === "ok") {
+            await startDb();
+            await ProductModel.findByIdAndUpdate(id, {
+                $pull: { images: { id: publicId } },
+            });
+        }
+    } catch (error) {
+        throw error;
+    }
+};
--- src/components/UpdateProduct.tsx
+++ src/components/UpdateProduct.tsx
@@ -2,6 +2,7 @@
 
 import { ProductResponse } from "@/types";
 import ProductForm, { InitialValue } from "./ProductForm";
+import { removeAndUpdateProductImage } from "@/app/(admin)/products/action";
 
 interface Props {
     product: ProductResponse;
@@ -16,7 +17,22 @@ const UpdateProduct = ({ product }: Props) => {
         bulletPoints: product.bulletPoints || [],
     };
 
-    return <ProductForm initialValue={initialValue} />;
+    const handleUpdateProduct = () => {};
+
+    const handleImageRemove = (source: string) => {
+        const spiltData = source.split("/");
+        const lastItem = spiltData[spiltData.length - 1];
+        const imagePublicId = lastItem.split(".")[0];
+        removeAndUpdateProductImage(product.id, imagePublicId);
+    };
+
+    return (
+        <ProductForm
+            onImageRemove={handleImageRemove}
+            initialValue={initialValue}
+            onSubmit={handleUpdateProduct}
+        />
+    );
 };
 
 export default UpdateProduct;
