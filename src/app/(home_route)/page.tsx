4af6e8ee8d15664d8a887fc94f81d2342a235a7c
Bipon Roy <biponroy400@gmail.com>
2024-04-18 21:06:05 +0600

    Added fetch product and render products functionality

--- src/app/(home_route)/page.tsx
+++ src/app/(home_route)/page.tsx
@@ -1,7 +1,46 @@
-export default function Home() {
+import GridView from "@/components/GridView";
+import startDb from "../lib/db";
+import ProductModel from "../models/productModel";
+import ProductCard from "@/components/ProductCard";
+
+interface ProductResponse {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    thumbnail: string;
+    price: {
+        base: number;
+        discounted: number;
+    };
+    sale: number;
+}
+
+const fetchProducts = async () => {
+    await startDb();
+    const products = await ProductModel.find().sort("-createdAt").limit(20);
+    const productList = products.map((prod) => {
+        return {
+            id: prod._id.toString(),
+            title: prod.title,
+            description: prod.description,
+            category: prod.category,
+            thumbnail: prod.thumbnail.url,
+            price: prod.price,
+            sale: prod.sale,
+        };
+    });
+
+    return JSON.stringify(productList);
+};
+export default async function Home() {
+    const products = await fetchProducts();
+    const parseProduct = JSON.parse(products) as ProductResponse[];
     return (
-        <>
-            <h1>Home Page</h1>
-        </>
+        <GridView>
+            {parseProduct.map((product) => {
+                return <ProductCard key={product.id} product={product} />;
+            })}
+        </GridView>
     );
 }
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,89 @@
+"use client";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    Typography,
+    Button,
+    CardFooter,
+    Chip,
+} from "@material-tailwind/react";
+import Image from "next/image";
+import Link from "next/link";
+import truncate from "truncate";
+
+interface Props {
+    product: {
+        id: string;
+        title: string;
+        description: string;
+        category: string;
+        thumbnail: string;
+        sale: number;
+        price: {
+            base: number;
+            discounted: number;
+        };
+    };
+}
+
+const ProductCard = ({ product }: Props) => {
+    return (
+        <Card placeholder={undefined} className="w-full rounded-md">
+            <CardHeader
+                placeholder={undefined}
+                shadow={false}
+                floated={false}
+                className="relative w-full aspect-square m-0 rounded-none"
+            >
+                <div>
+                    <Image src={product.thumbnail} alt={product.title} fill />
+                </div>
+                <div className="absolute right-0 p-2">
+                    <Chip color="red" value={`${product.sale}% off`} />
+                </div>
+            </CardHeader>
+            <CardBody placeholder={undefined}>
+                <div className="mb-2">
+                    <h3 className="line-clamp-1 font-medium text-blue-gray-800">
+                        {truncate(product.title, 50)}
+                    </h3>
+                </div>
+                <div className="flex justify-end items-center space-x-2 mb-2">
+                    <Typography
+                        placeholder={undefined}
+                        color="blue-gray"
+                        className="font-medium line-through"
+                    >
+                        ${product.price.base}
+                    </Typography>
+                    <Typography placeholder={undefined} color="blue-gray" className="font-medium">
+                        ${product.price.discounted}
+                    </Typography>
+                </div>
+                <p className="font-normal text-sm opacity-75 line-clamp-3">{product.description}</p>
+            </CardBody>
+
+            <CardFooter placeholder={undefined} className="pt-0 space-y-4">
+                <Button
+                    placeholder={undefined}
+                    ripple={false}
+                    fullWidth={true}
+                    className="bg-blue-gray-900/10 text-blue-gray-900 shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+                >
+                    Add to Cart
+                </Button>
+                <Button
+                    placeholder={undefined}
+                    ripple={false}
+                    fullWidth={true}
+                    className="bg-blue-400 text-white shadow-none hover:shadow-none hover:scale-105 focus:shadow-none focus:scale-105 active:scale-100"
+                >
+                    Buy Now
+                </Button>
+            </CardFooter>
+        </Card>
+    );
+};
+
+export default ProductCard;
