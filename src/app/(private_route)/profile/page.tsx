18d9b4f106b4370a213d942c26b9dc1107e3bb91
Bipon Roy <biponroy400@gmail.com>
2024-05-04 19:14:56 +0600

    Added fetchUserProfile function on user profile page

--- src/app/(private_route)/profile/page.tsx
+++ src/app/(private_route)/profile/page.tsx
@@ -1,7 +1,35 @@
-const page = () => {
+import startDb from "@/app/lib/db";
+import UserModel from "@/app/models/userModel";
+import { auth } from "@/auth";
+import ProfileForm from "@/components/ProfileForm";
+import { redirect } from "next/navigation";
+
+const fetchUserProfile = async () => {
+    const session = await auth();
+    if (!session) return redirect("/auth/signin");
+
+    await startDb();
+    const user = await UserModel.findById(session.user.id);
+    if (!user) return redirect("/auth/signin");
+    return {
+        id: user._id.toString(),
+        name: user.name,
+        email: user.email,
+        avatar: user.avatar?.url,
+        verified: user.verified,
+    };
+};
+
+const page = async () => {
+    const profile = await fetchUserProfile();
     return (
         <div>
-            <h1>Profile</h1>
+            <ProfileForm
+                id={profile.id}
+                avatar={profile.avatar}
+                email={profile.email}
+                name={profile.name}
+            />
         </div>
     );
 };
