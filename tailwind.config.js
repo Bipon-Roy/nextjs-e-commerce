dee6dd6e915cbf28cf755d06b405b5d7dbb05eca
Bipon Roy <biponroy400@gmail.com>
2024-06-17 18:23:28 +0600

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,14 @@ module.exports = withMT({
     ],
     theme: {
         extend: {},
+        screens: {
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+        },
     },
+    variants: {},
     plugins: [],
 });
